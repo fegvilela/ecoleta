e69c2d7726086286356adf8956925e9f7fbd037b
fegvilela <fegvilela@gmail.com>
2020-06-06 02:20:43 -0300

    add schema and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log("Listagem de usuários");
-
-  response.json([
-    'Fernanda',
-    'Isabela',
-    'Lulu'
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
